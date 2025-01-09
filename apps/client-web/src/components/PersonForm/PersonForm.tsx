052e505907c8198f75b02c53a0be10af8ab78e4b
Josh Smith <josh@leapsake.com>
2025-01-09 16:53:52 -0500

    Create PersonForm, centralize logic in it

---
+++ apps/client-web/src/components/PersonForm/PersonForm.tsx
@@ -0,0 +1,32 @@
+import BaseInput from '@/components/BaseInput';
+import DateInput from '@/components/DateInput';
+
+export default function PersonForm({
+	action,
+	buttonText = 'Submit',
+	person = {},
+}) {
+	return (
+		<form action={action}>
+			<BaseInput
+				defaultValue={person.given_name}
+				label="First Name"
+				name="given_name"
+			/>
+
+			<BaseInput
+				defaultValue={person.family_name}
+				label="Last Name"
+				name="family_name"
+			/>
+
+			<DateInput
+				defaultValue={person.birth_date}
+				label="Birthday"
+				name="birth_date"
+			/>
+
+			<button type="submit">{buttonText}</button>
+		</form>
+	);
+}
