d06a54a466769a354629a5b562b52b34c3ce62f8
Josh Smith <josh@leapsake.com>
2025-01-10 13:04:28 -0500

    Create new table for PersonDates

--- apps/client-web/src/db/index.ts
+++ apps/client-web/src/db/index.ts
@@ -10,6 +10,16 @@ db.serialize(() => {
 		given_name TEXT,
 		family_name TEXT
 	)`);
+
+	db.run(`CREATE TABLE IF NOT EXISTS PersonDates(
+		id TEXT PRIMARY KEY,
+		people_id TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		year TEXT,
+		month TEXT,
+		day TEXT
+	)`);
 });
 
 export default db;
