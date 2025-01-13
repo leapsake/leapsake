6dc89f2099e3d40663371a06654d647ec162e03e
Josh Smith <josh@leapsake.com>
2025-01-13 15:54:05 -0500

    Add milestone addition functionality

--- apps/client-web/src/app/people/[id]/page.tsx
+++ apps/client-web/src/app/people/[id]/page.tsx
@@ -1,39 +1,49 @@
 import { Fragment } from 'react';
 import Link from 'next/link';
 import { readPerson } from '@/db/people';
+import { browseMilestones } from '@/db/milestones';
 
 export default async function ReadPersonPage({ params }: { params: Promise<{ id: string }> }) {
-	const id = (await params).id;
-	const person = await readPerson(id);
+	const person_id = (await params).id;
+	const person = await readPerson(person_id);
+	const milestones = await browseMilestones(person_id);
 
 	return (
 		<Fragment>
 			<header>
 				<h1>{person.given_name} {person.family_name}</h1>
 				<ul>
-					<li><Link href={`/people/${id}/edit`}>📝 {`Edit ${person.given_name}`}</Link></li>
-					<li><Link href={`/people/${id}/delete`}>🗑️ {`Delete ${person.given_name}`}</Link></li>
+					<li><Link href={`/people/${person_id}/edit`}>📝 {`Edit ${person.given_name}`}</Link></li>
+					<li><Link href={`/people/${person_id}/delete`}>🗑️ {`Delete ${person.given_name}`}</Link></li>
 				</ul>
 			</header>
 
 			<section>
 				<h2>Milestones</h2>
 
-				{!!person.birth_date
+				{!!milestones.length
 					? (
 						<Fragment>
-							<h3>🎂 Born</h3>
-							<div>{person.birth_date}</div>
+							<ul>
+								{milestones.map((milestone) => (
+									<li>
+										<h3>{milestone.type}</h3>
+										<time datetime={`${milestone.year}-${milestone.month}-${milestone.day}`}>{`${milestone.year}-${milestone.month}-${milestone.day}`}</time>
+									</li>
+								))}
+							</ul>
+							<Link href={`/people/${person_id}/milestones/edit`}>➕ Add another Milestone</Link>
 						</Fragment>
 					)
 					: (
-						<Link href={`/people/${id}/milestones/edit?type=birth`}>➕ Add a Birthday</Link>
+						<Link href={`/people/${person_id}/milestones/edit?type=birth`}>➕ Add a Birthday</Link>
 					)
 				}
 			</section>
 
 			<footer>
-				<i>Created: {person.created_at}<br/>Last Updated: {person.updated_at}</i>
+				<i>Created: <time datetime={person.created_at}>{person.created_at}</time>
+					<br/>Last Updated: <time datetime={person.updated_at}>{person.updated_at}</time></i>
 			</footer>
 		</Fragment>
 	);
--- apps/client-web/src/db/milestones.ts
+++ apps/client-web/src/db/milestones.ts
@@ -2,9 +2,15 @@ import db from '@/db';
 import { v4 as uuidv4 } from 'uuid';
 import { redirect } from 'next/navigation';
 
-export async function browseMilestones() {
+export async function browseMilestones(person_id) {
 	const milestones = await new Promise((resolve, reject) => {
-		db.all(`SELECT * FROM Milestones`, (err, rows) => {
+		db.all(`SELECT *
+				FROM Milestones
+				WHERE
+					(person_id = ?)
+				ORDER BY year ASC, month ASC, day ASC`,
+			[ person_id ],
+			(err, rows) => {
 			if (err) {
 				reject(err);
 			} else {
@@ -15,9 +21,9 @@ export async function browseMilestones() {
 	return milestones;
 }
 
-export async function readMilestone(id: string) {
+export async function readMilestone(milestone_id: string) {
 	const person = await new Promise((resolve, reject) => {
-		db.get(`SELECT * FROM Milestones WHERE id = ?`, [ id ], (err, row) => {
+		db.get(`SELECT * FROM Milestones WHERE id = ?`, [ milestone_id ], (err, row) => {
 			if (err) {
 				reject(err);
 			} else {
@@ -38,20 +44,28 @@ export async function addMilestone(personId: string, formData: FormData) {
 	const year = formData.get('year');
 	const milestoneId = uuidv4();
 
-	db.serialize(() => {
-		db.run(`INSERT INTO Milestones(id, day, label, month, person_id, type, year) VALUES(?, ?, ?, ?, ?, ?, ?)`, [
-			milestoneId,
-			day,
-			label,
-			month,
-			personId,
-			type,
-			year,
-		], (err) => {
-			if (err) {
-				console.error(err);
-			}
-		});
+	db.run(`INSERT INTO Milestones(id, day, label, month, person_id, type, year) VALUES(?, ?, ?, ?, ?, ?, ?)`, [
+		milestoneId,
+		day,
+		label,
+		month,
+		personId,
+		type,
+		year,
+	], (err) => {
+		if (err) {
+			console.error(err);
+		}
+	});
+
+	db.run(`
+		UPDATE People
+		SET updated_at = datetime('now')
+		WHERE id = ?
+	`, [ personId ], (err) => {
+		if (err) {
+			console.error(err);
+		}
 	});
 
 	redirect(`/people/${personId}`);
