5bed6077de40fe02cc8d092b1263d3938581c49a
Josh Smith <josh@leapsake.com>
2025-01-14 00:52:34 -0500

    Add getPrettyDate util

---
+++ apps/client-web/src/utils.ts
@@ -0,0 +1,28 @@
+export function getPrettyDate(day, month, year) {
+	const monthLabel = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	][Number(month) - 1];
+
+	const dayLabel = Number(day);
+
+	if (monthLabel && dayLabel && year) {
+		return `${monthLabel} ${dayLabel}, ${year}`;
+	} else if (monthLabel && dayLabel) {
+		return `${monthLabel} ${dayLabel}`;
+	} else if (monthLabel && year) {
+		return `${monthLabel} ${year}`;
+	} else {
+		return monthLabel || year;
+	}
+}
