0bdc46bf783deb9cb907ce79038234596fe0d4ec
Josh Smith <josh@leapsake.com>
2025-01-14 00:26:12 -0500

    Add delete milestone functionality

---
+++ apps/client-web/src/app/people/[personId]/milestones/[milestoneId]/delete/page.tsx
@@ -0,0 +1,19 @@
+import { readPerson } from '@/db/people';
+import { deleteMilestone, readMilestone } from '@/db/milestones';
+import { Fragment } from 'react';
+
+export default async function DeleteMilestonePage({ params }: { params: Promise<{ milestoneId: string }> }) {
+	const { milestoneId, personId } = await params;
+	const person = await readPerson(personId);
+	const milestone = await readMilestone(milestoneId);
+
+	return (
+		<Fragment>
+			<h1>{`Delete ${person.given_name}'s ${milestone.label}z`}</h1>
+
+			<form action={deleteMilestone.bind(null, milestoneId, personId)}>
+				<button type="submit">Delete</button>
+			</form>
+		</Fragment>
+	);
+}
