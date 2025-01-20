e2c016cd51cbeff3900ed5364863a4f814a4a491
Josh Smith <josh@leapsake.com>
2025-01-19 19:37:34 -0500

    Add PhoneNumberForm

---
+++ apps/client-web/src/components/PhoneNumberForm/PhoneNumberForm.tsx
@@ -0,0 +1,32 @@
+import BaseInput from '@/components/BaseInput';
+
+export default function PhoneNumberForm({
+	action,
+	buttonText = 'Submit',
+	phoneNumber = {},
+}) {
+	return (
+		<form action={action} name="phone-number">
+			<BaseInput
+				defaultValue={phoneNumber.label}
+				label="Label"
+				list="number-labels"
+				name="label"
+			/>
+
+			<datalist id="number-labels">
+				<option value="Home"></option>
+				<option value="Mobile"></option>
+				<option value="Work"></option>
+			</datalist>
+
+			<BaseInput
+				defaultValue={phoneNumber.number}
+				label="Phone Number"
+				name="number"
+			/>
+
+			<button type="submit">{buttonText}</button>
+		</form>
+	);
+}
