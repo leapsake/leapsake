f27cf33d0b3517b90cdfce6057fc9c80e59d210b
Josh Smith <josh@leapsake.com>
2025-02-05 16:19:35 -0500

    Separate db init logic

--- apps/client-web/src/db/index.ts
+++ apps/client-web/src/db/index.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import sqlite3 from 'sqlite3';
+import initDB from './init';
 
 const dataDir = path.resolve('data');
 const databasePath = path.resolve(dataDir, 'development.db');
@@ -8,52 +9,6 @@ const databasePath = path.resolve(dataDir, 'development.db');
 
 const db = new sqlite3.Database(databasePath);
 
-db.serialize(() => {
-	db.run(`CREATE TABLE IF NOT EXISTS People(
-		id TEXT PRIMARY KEY,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		family_name TEXT,
-		given_name TEXT,
-		maiden_name TEXT,
-		middle_name TEXT
-	)`);
-
-	db.run(`CREATE TABLE IF NOT EXISTS Milestones(
-		id TEXT PRIMARY KEY,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		day TEXT,
-		label TEXT,
-		month TEXT,
-		person_id TEXT,
-		year TEXT
-	)`);
-
-	db.run(`CREATE TABLE IF NOT EXISTS EmailAddresses(
-		id TEXT PRIMARY KEY,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		address TEXT,
-		label TEXT,
-		person_id TEXT
-	)`);
-
-	db.run(`CREATE TABLE IF NOT EXISTS PhoneNumbers(
-		id TEXT PRIMARY KEY,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		label TEXT,
-		number TEXT,
-		person_id TEXT
-	)`);
-
-	db.run(`CREATE TABLE IF NOT EXISTS Photos(
-		id TEXT PRIMARY KEY,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		path TEXT
-	)`);
-});
+initDB(db);
 
 export default db;
