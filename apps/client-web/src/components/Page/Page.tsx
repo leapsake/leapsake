12521e059a024b1fe03a7b642a5ee47f5fc5a8e6
Josh Smith <josh@leapsake.com>
2025-04-16 11:40:12 -0400

    Fix page header boolean operation

--- apps/client-web/src/components/Page/Page.tsx
+++ apps/client-web/src/components/Page/Page.tsx
@@ -10,7 +10,7 @@ export default function Page({
 
 	return (
 		<Fragment>
-			{headerIsVisible ?? (
+			{headerIsVisible && (
 				<header className={styles.header}>
 					<div className={styles.headerContent}>
 						<h1>{title}</h1>
