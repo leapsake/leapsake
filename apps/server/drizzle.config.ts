434df33ed35299c0bdde12248cd9c3d75c29447e
Josh Smith <josh@leapsake.com>
2025-07-15 23:05:33 -0400

    Create drizzle config

---
+++ apps/server/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema/*',
+  out: './src/db/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: parseInt(process.env.POSTGRES_PORT || '5432'),
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || '',
+    database: process.env.POSTGRES_DB || 'leapsake',
+  },
+});
