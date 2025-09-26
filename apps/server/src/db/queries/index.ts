f7725f03e5a6856d51a4332e6bcd6ced98aafdf6
Josh Smith <josh@leapsake.com>
2025-09-25 21:11:33 -0400

    Restarting the project

--- apps/server/src/db/queries/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './email-addresses.js';
-export * from './milestones.js';
-export * from './people.js';
-export * from './photos.js';
-export * from './phone-numbers.js';
--- apps/server/src/db/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './people';
-export * from './milestones';
-export * from './email-addresses';
-export * from './phone-numbers';
-export * from './photos';
-export * from './albums';
