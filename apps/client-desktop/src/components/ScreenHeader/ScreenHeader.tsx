3030c403dfb9f63bdb4ac58b51ad6565d329e48f
Josh Smith <josh@leapsake.com>
2025-10-31 01:18:31 -0400

    Reorganize components, add styles

--- apps/client-desktop/src/components/ScreenHeader/ScreenHeader.tsx
+++ apps/client-desktop/src/components/ScreenHeader/ScreenHeader.tsx
@@ -1,16 +1,46 @@
 import { type ComponentChildren } from 'preact';
 import styles from './ScreenHeader.module.css';
 
+interface Breadcrumb {
+	label: string;
+	url: string;
+}
+
 interface ScreenHeaderProps {
 	children: ComponentChildren;
 	title: string;
+	breadcrumbs?: Breadcrumb[];
 };
 
-export function ScreenHeader({ children, title }: ScreenHeaderProps) {
+export function ScreenHeader({ children, title, breadcrumbs }: ScreenHeaderProps) {
 	return (
-		<header class={styles.header}>
-			<h1>{title}</h1>
-			{children}
+		<header>
+			<Breadcrumbs>
+				{breadcrumbs}
+			</Breadcrumbs>
+
+			<div class={styles.content}>
+				<h1>{title}</h1>
+				{children}
+			</div>
 		</header>
+	);
+}
+
+function Breadcrumbs({ children }: { children?: Breadcrumb[] }) {
+	if (!children || children.length < 1) {
+		return null;
+	}
+	
+	return (
+		<ol class={styles.breadcrumbs}>
+			{children.map((breadcrumb) => (
+				<li>
+					<a href={breadcrumb.url}>
+						{breadcrumb.label}
+					</a>
+				</li>
+			))}
+		</ol>
 	);
 }
