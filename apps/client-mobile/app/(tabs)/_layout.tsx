1a0fbd01bc846c1806b5ce9e6660c2f192c4384d
Josh Smith <josh@leapsake.com>
2025-11-11 00:38:12 -0500

    Remove boilerplate from mobile client

--- apps/client-mobile/app/(tabs)/_layout.tsx
+++ apps/client-mobile/app/(tabs)/_layout.tsx
@@ -20,14 +20,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+				tabBarIcon: ({ color }) => <IconSymbol size={28} name="" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+				tabBarIcon: ({ color }) => <IconSymbol size={28} name="" color={color} />,
         }}
       />
     </Tabs>
