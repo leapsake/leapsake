41160fec1cab9984625710bdbc6f39bbb3df98f5
Josh Smith <josh@leapsake.com>
2025-11-10 22:20:03 -0500

    Remove unused pages and links

--- apps/leapsake.com/src/components/SocialMediaLinks/SocialMediaLinks.tsx
+++ apps/leapsake.com/src/components/SocialMediaLinks/SocialMediaLinks.tsx
@@ -1,14 +1,18 @@
 export function SocialMediaLinks() {
 	return (
 		<ul>
-			<li><a href="https://bsky.app/profile/leapsake.bsky.social">Bluesky</a></li>
+			{/* 
+				<li><a href="https://bsky.app/profile/leapsake.bsky.social">Bluesky</a></li>
+				<li><a href="https://www.facebook.com/leapsake">Facebook</a></li>
+			*/}
 			<li><a href="https://github.com/leapsake">GitHub</a></li>
-			<li><a href="https://mastodon.social/@leapsake">Mastodon</a></li>
-			<li><a href="https://www.facebook.com/leapsake">Facebook</a></li>
-			<li><a href="https://www.instagram.com/leapsake/">Instagram</a></li>
-			<li><a href="https://www.tiktok.com/@leapsake">TikTok</a></li>
-			<li><a href="https://x.com/leapsake">X</a></li>
-			<li><a href="https://www.youtube.com/@leapsake">YouTube</a></li>
+			{/*
+				<li><a href="https://www.instagram.com/leapsake/">Instagram</a></li>
+				<li><a href="https://mastodon.social/@leapsake">Mastodon</a></li>
+				<li><a href="https://www.tiktok.com/@leapsake">TikTok</a></li>
+				<li><a href="https://x.com/leapsake">X</a></li>
+				<li><a href="https://www.youtube.com/@leapsake">YouTube</a></li>
+			 */}
 		</ul>
 	);
 }
