1519ae0f41ebd5624ff67c40423bb6ef5246879e
Josh Smith <josh@leapsake.com>
2025-12-27 00:47:02 -0500

    Use new Tauri commands for reading from SQLite

--- apps/client-desktop/src/types.ts
+++ apps/client-desktop/src/types.ts
@@ -28,3 +28,70 @@ export interface PartialDate {
 	month?: number;
 	day?: number;
 }
+
+// Database types (new SQLite-backed API)
+
+export interface Person {
+	id: string;
+	given_name?: string;
+	middle_name?: string;
+	family_name?: string;
+	birthday?: PartialDate;
+	anniversary?: PartialDate;
+	photo?: string;
+	organization?: string;
+	title?: string;
+	url?: string;
+	note?: string;
+	created_at: number;
+	updated_at: number;
+}
+
+export interface EmailAddress {
+	id: string;
+	person_id: string;
+	email: string;
+	label?: string;
+	position: number;
+}
+
+export interface PhoneNumber {
+	id: string;
+	person_id: string;
+	number: string;
+	label?: string;
+	features?: string[];
+	position: number;
+}
+
+export interface Address {
+	id: string;
+	person_id: string;
+	street: string;
+	locality?: string;
+	region?: string;
+	postcode?: string;
+	country?: string;
+	label?: string;
+	position: number;
+}
+
+export interface PersonWithDetails {
+	person: Person;
+	emails: EmailAddress[];
+	phones: PhoneNumber[];
+	addresses: Address[];
+}
+
+export interface NewPerson {
+	given_name?: string;
+	middle_name?: string;
+	family_name?: string;
+	birthday?: PartialDate;
+	anniversary?: PartialDate;
+	photo?: string;
+	organization?: string;
+	title?: string;
+	url?: string;
+	note?: string;
+}
